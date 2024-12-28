0b5dab708d7dbc01cb77950c172bd7a5fec04284
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-12-28 21:08:25 +0100

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,29 @@
+
+using System;
+
+public class Solution
+{
+    private static readonly int NOT_A_CONFUSING_NUMBER = -1;
+    private static readonly int[] MAP_ROTATED_CONFUSING_NUMBERS
+                = {0 /*0 => 0*/, 1 /*1 => 1*/, NOT_A_CONFUSING_NUMBER, NOT_A_CONFUSING_NUMBER,
+                NOT_A_CONFUSING_NUMBER, NOT_A_CONFUSING_NUMBER, 9/*6 => 9*/,
+                NOT_A_CONFUSING_NUMBER, 8/*8 => 8*/, 6/*9 => 6*/};
+
+    public bool ConfusingNumber(int inputValue)
+    {
+        int copyInputValue = inputValue;
+        int rotatedInputValue = 0;
+
+        while (copyInputValue > 0)
+        {
+            int digit = copyInputValue % 10;
+            if (MAP_ROTATED_CONFUSING_NUMBERS[digit] == NOT_A_CONFUSING_NUMBER)
+            {
+                return false;
+            }
+            copyInputValue /= 10;
+            rotatedInputValue = rotatedInputValue * 10 + MAP_ROTATED_CONFUSING_NUMBERS[digit];
+        }
+        return rotatedInputValue != inputValue;
+    }
+}
